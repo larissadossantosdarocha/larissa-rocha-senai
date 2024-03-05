b3ffd7947f15faa1e7af0336e6499767a6a8b339
larissadossantosdarocha <162310756+larissadossantosdarocha@users.noreply.github.com>
2024-03-05 16:43:14 -0300

    Create nota.c

---
+++ nota.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+int main()  /*função principal*/ {
+   https://www.onlinegdb.com/online_c++_compiler#editor_1 float soma,nota1,nota2,nota3,media;  soma=0;  media=0;        
+    printf("\t Digite a [1]ª  nota : ");
+       scanf("%f",&nota1);
+
+       printf("\t Digite a [2]ª  nota : ");
+       scanf("%f",&nota2);
+       printf("\t Digite a [3]ª  nota : ");
+       scanf("%f",&nota3);
+
+       soma=nota1+nota2+nota3;
+       media=soma/3;  printf("Media = %.1f ",media );
+ return 0;
+}
